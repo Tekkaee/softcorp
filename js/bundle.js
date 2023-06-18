ff7b2db64ba932b8c4a5ad9aafddea0c08f63b94
Tekkaee <tekkaee@gmail.com>
2023-06-18 22:37:53 +0000

    Deploying to gh-pages from @ Tekkaee/softcorp@7edd85e0d79a1cd814fc42fb193ae16e7da6d4f2 🚀

--- js/bundle.js
+++ js/bundle.js
@@ -187,7 +187,6 @@ const observe = (select) => {
     }
 
     if (opened) {
-        console.warn(`OPENED ${shouldBeOpenedToTop} ${currentMode}`)
         if(shouldBeOpenedToTop && currentMode === 'bottom') {
 
             select.classList.remove(openedBottomClass.substring(1))
